8c708a2550e96440ee2b224d7f10b0807bb24a0c
merylschoice22 <kekrieg.digital@gmail.com>
2021-06-02 21:44:30 +0200

    Return quote by ID

--- server.js
+++ server.js
@@ -3,11 +3,13 @@ const app = express();
 
 const quotes = require("./quotes.json");
 
-app.get('/', function(request, response) {
-  response.send('/quotes/17 should return one quote, by id')
+app.get("/quotes/:id", function (request, response) {
+  //'/quotes/17 should return one quote, by id'
+  const index = parseInt(request.params.id) - 1;
+  response.send(quotes[index]);
 });
 
-app.get("/quotes", function(request, response){
+app.get("/quotes", function (request, response) {
   response.json(quotes);
 });
 
